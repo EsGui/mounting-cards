5f148e011ae0d0c72670ec7d421417162293f178
EsGui <guilhermedagama2021100@gmail.com>
2024-04-26 20:21:50 -0300

    Funções de deletar e adicionar cards manipulando localstorage foi finalizado

--- src/app/components/Body.js
+++ src/app/components/Body.js
@@ -7,24 +7,34 @@ export default function Body() {
     const [cardName, setCardName] = useState("");
     const [urlImage, setUrlImage] = useState("");
     const [descricao, setDescricao] = useState("");
+    const [listCards, setListCards] = useState([]);
 
     useEffect(() => {
         if (!localStorage.getItem("listCards")) {
             localStorage.setItem("listCards", JSON.stringify([]));
         }
+        setListCards(JSON.parse(localStorage.getItem("listCards")))
     }, []);
 
     const createCard = () => {
         const listCards = JSON.parse(localStorage.getItem("listCards"))
         localStorage.setItem("listCards", JSON.stringify([...listCards, 
-            { id: listCards.length, 
+            { 
+                id: listCards.length, 
                 name: cardName, 
                 image: urlImage, 
                 desc: descricao
-            }]))
+            }
+        ]));
+        setListCards(JSON.parse(localStorage.getItem("listCards")))
     }
 
-    const listCards = JSON.parse(localStorage.getItem("listCards"))
+    const deleteCard = (id) => {
+        const listCards = JSON.parse(localStorage.getItem("listCards"));
+        const newList = listCards.filter((element) => Number(element.id) != Number(id));
+        localStorage.setItem("listCards", JSON.stringify(newList));
+        setListCards(JSON.parse(localStorage.getItem("listCards")))
+    }
 
     return (
         <>
@@ -60,25 +70,21 @@ export default function Body() {
             </div>
             <div className={ styles.DivCardSave }>
                 {
-                    listCards && listCards.map(({ name, image, desc }) => (
+                    listCards && listCards.map(({ id, name, image, desc }) => (
                         <div className={ styles.DivCardBody }>
                             <div className={styles.TitleCard}>
                                 <p>{name}</p>
                             </div>
                             <div className={styles.DivImageCard}>
-                                {
-                                    urlImage.length > 0 ?
-                                        <img src={image} />
-                                    :
-                                        <p>Adicione uma imagem pelo campo de url</p>
-                                }
-                                
+                                <img src={image} />
+                                <p>Adicione uma imagem pelo campo de url</p>
                             </div>
                             <div className={ styles.DivDescriptionCard }>
                                 <p>Descrição:</p>
                                 <p>{desc}</p>
                             </div>
-                            <div className={styles.DivButtonCreate}>
+                            <div className={styles.DivButtonDelete}>
+                                <button onClick={() => deleteCard(id)} type="button">Excluir</button>
                             </div>
                         </div>
                     ))
